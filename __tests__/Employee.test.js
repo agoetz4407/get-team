7d6da2cc9d7209b91557caaffac7b43538793bc9
Andrew Goetz <agoetz4407@gmail.com>
2022-05-02 18:45:45 -0500

    employee class test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,3 +7,27 @@ test('creates an employee object', () => {
     expect(employee.id).toBe('4407')
     expect(employee.email).toBe('agoetz4407@gmail.com')
 })
+
+test('gets employee name', () => {
+    const employee = new Employee('Andrew', '4407', 'agoetz4407@gmail.com')
+
+    expect(employee.getName()).toBe('Andrew')
+})
+
+test('gets employee ID', () => {
+    const employee = new Employee('Andrew', '4407', 'agoetz4407@gmail.com')
+
+    expect(employee.getId()).toBe('4407')
+})
+
+test('gets employee email address', () => {
+    const employee = new Employee('Andrew', '4407', 'agoetz4407@gmail.com')
+
+    expect(employee.getEmail()).toBe('agoetz4407@gmail.com')
+})
+
+test('gets employee role', () => {
+    const employee = new Employee('Andrew', '4407', 'agoetz4407@gmail.com')
+
+    expect(employee.getRole()).toBe('Employee')
+})
