fb2d2ac195574aa0cb69a535c780aceaaefcabe0
Andrew Goetz <agoetz4407@gmail.com>
2022-05-02 21:57:53 -0500

    prompts finished with array of team members

--- index.js
+++ index.js
@@ -6,19 +6,27 @@ const Intern = require('./lib/Intern')
 
 const team = []
 
+const generateTeam = function(){
+    team.forEach(member => console.log(member))
+}
+
 const generateManager = function(managerInfo) {
     const manager = new Manager(managerInfo.name, managerInfo.id, managerInfo.email, managerInfo.officeNumber)
     team.push(manager)
+    console.log(`${manager.name} is added to team!`)
 }
 
 const generateEngineer = function(engineerInfo) {
     const engineer = new Engineer(engineerInfo.name, engineerInfo.id, engineerInfo.email, engineerInfo.github)
     team.push(engineer)
+    console.log(`${engineer.name} is added to the team!`)
+
 }
 
 const generateIntern = function(internInfo) {
     const intern = new Intern(internInfo.name, internInfo.id, internInfo.email, internInfo.school)
     team.push(intern)
+    console.log(`${intern.name} is added to the team!`)
 }
 
 const engineerPrompt = function() {
@@ -83,17 +91,79 @@ const engineerPrompt = function() {
     })
 }
 
+const internPrompt = function() {
+    inquirer.prompt([
+        {
+            type:'input',
+            name: 'name',
+            message: "What is the intern's name?",
+            validate: input => {
+                if (input) {
+                    return true
+                } else {
+                    console.log("Please enter the intern's name")
+                    return false
+                }
+            }
+        },
+        {
+            type:'input',
+            name: 'id',
+            message: "What is the intern's employee ID?",
+            validate: input => {
+                if (input) {
+                    return true
+                } else {
+                    console.log("Please enter the intern's employee ID")
+                    return false
+                }
+            }
+        },
+        {
+            type:'input',
+            name: 'email',
+            message: "What is the intern's email address?",
+            validate: input => {
+                if (!input || !input.includes('@') || !input.includes('.')) {
+                    console.log(' Please enter a valid email address')
+                    return false
+                }
+                else {
+                    return true
+                }
+            }
+        },
+        {
+            type:'input',
+            name: 'school',
+            message: "What school is the intern associated with?",
+            validate: input => {
+                if (input) {
+                    return true
+                } else {
+                    console.log("Please enter the intern's school")
+                    return false
+                }
+            }
+        }
+    ])
+    .then(intern => {
+        generateIntern(intern)
+        teamPrompt()
+    })
+}
+
 const teamPrompt = function() {
     inquirer.prompt([
         {
             type: 'list',
             name: 'teamChoice',
-            message: 'Choose team member role to add, or select "Done" to finish',
+            message: 'Choose team member to add, or select "Done" to finish',
             choices: ['Engineer', 'Intern', 'Done']
         }
     ])
     .then(choice => {
-        switch(choice){
+        switch(choice.teamChoice){
             case 'Engineer':
                 engineerPrompt()
                 break;
